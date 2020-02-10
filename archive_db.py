7208413a63e9bc42f0a3054fae5432e793f6041d
Sam Sciolla <ssciolla@umich.edu>
2020-02-10 10:39:58 -0500

    Change OUT_DIR to OUT_PATH

--- archive_db.py
+++ archive_db.py
@@ -21,7 +21,7 @@ except FileNotFoundError:
 logger.setLevel(ENV.get('LOGGING_LEVEL', 'DEBUG'))
 logger.info("** archive-db.py **")
 
-OUT_DIR = ENV.get('OUT_DIR', 'data')
+OUT_PATH = ENV.get('OUT_PATH', 'data')
 
 DB_PARAMS = ENV['MYSQL_DATABASE']
 ENGINE = create_engine(
@@ -36,13 +36,13 @@ logger.info(f"Created engine for communicating with {DB_PARAMS['DATABASE']} data
 
 # Function(s)
 
-def write_tables_as_csvs(out_path: str) -> None: 
+def write_tables_as_csvs(root_path: str) -> None: 
     table_names_series = pd.read_sql('SHOW TABLES;', ENGINE).iloc[:, 0]
     logger.debug(table_names_series)
     for table_name in table_names_series.to_list():
         table_df = pd.read_sql(table_name, ENGINE)
         logger.debug(table_df.head())
-        csv_file_path = f'{out_path}/{table_name}.csv'
+        csv_file_path = f'{root_path}/{table_name}.csv'
         table_df.to_csv(csv_file_path, index=False)
         logger.info(f'Wrote {table_name} table to {csv_file_path}')
 
@@ -52,14 +52,14 @@ def write_tables_as_csvs(out_path: str) -> None:
 if __name__ == '__main__':
     current_str_time = datetime.now().isoformat(timespec='seconds')
 
-    if not os.path.isdir(OUT_DIR):
-        os.makedirs(OUT_DIR)
-        logger.info(f'Created OUT_DIR {OUT_DIR} (and intermediate directories) because it did not exist')
+    if not os.path.isdir(OUT_PATH):
+        os.makedirs(OUT_PATH)
+        logger.info(f'Created directory (or directories) for OUT_PATH {OUT_PATH}')
     else:
-        logger.info(f'OUT_DIR {OUT_DIR} already exists')
+        logger.info(f'OUT_PATH {OUT_PATH} already exists')
 
     archive_dir_name = f"archive-{DB_PARAMS['DATABASE']}-{current_str_time}"
-    archive_path = f'{OUT_DIR}/{archive_dir_name}'
+    archive_path = f'{OUT_PATH}/{archive_dir_name}'
     os.mkdir(archive_path)
 
     write_tables_as_csvs(archive_path)
